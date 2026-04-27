d2a9e16386a6a675254243d0744984a0e217ca3f
Ahnik <sarkarahnik@gmail.com>
2026-04-27 09:50:28 +0530

    Added a way to print the directory structure of any directory passed as argument

--- src/main.c
+++ src/main.c
@@ -5,14 +5,22 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv) {
+    // Variables used
     DIR *directory;
     struct dirent *entry;
+    const char *dir_path;
+
+    // Set the directory path
+    if (argc == 1)
+        dir_path = ".";
+    else
+        dir_path = argv[1];
 
     // Set errno to 0
     errno = 0;
 
     // Open the directory stream
-    directory = opendir(".");
+    directory = opendir(dir_path);
 
     if (directory == NULL) {
         perror("Error opening directory directory");
@@ -24,11 +32,13 @@ int main(int argc, char **argv) {
         printf("%s\n", entry->d_name);
     }
 
+    // Close the directory stream
+    closedir(directory);
+
     if (errno != 0) {
         perror("Error reading directory");
         return EXIT_FAILURE;
     }
 
-    closedir(directory);
     return EXIT_SUCCESS;
 }

40f30ecbe224be6650bec8c47b493d7967d1e629
Ahnik <sarkarahnik@gmail.com>
2026-04-27 11:54:02 +0530

    phase 1 and 2 complete

--- src/main.c
+++ src/main.c
@@ -1,20 +1,36 @@
-#include <sys/types.h>
+#define _POSIX_C_SOURCE 200809L
+
 #include <dirent.h>
 #include <errno.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char **argv) {
     // Variables used
     DIR *directory;
     struct dirent *entry;
-    const char *dir_path;
+    const char *dir_path = ".";
+    int opt;
+    int a_flag = 0;
 
-    // Set the directory path
-    if (argc == 1)
-        dir_path = ".";
-    else
-        dir_path = argv[1];
+    // Check if the -a flag is set
+    while ((opt = getopt(argc, argv, "a")) != -1) {
+        switch(opt) {
+            case 'a':
+                a_flag = 1;
+                break;
+            case '?':
+                return 1;
+            default:
+                abort();
+        }
+    }
+
+    if (optind < argc) {
+        dir_path = argv[optind];
+    }
 
     // Set errno to 0
     errno = 0;
@@ -23,22 +39,24 @@ int main(int argc, char **argv) {
     directory = opendir(dir_path);
 
     if (directory == NULL) {
-        perror("Error opening directory directory");
-        return EXIT_FAILURE;
+        perror(argv[0]);
+        return 1;
     }
 
     // Read all entries in the directory stream
     while ((entry = readdir(directory)) != NULL) {
-        printf("%s\n", entry->d_name);
+        if (strlen(entry->d_name) > 0 && entry->d_name[0] != '.')
+            printf("%s  ", entry->d_name);
     }
+    printf("\n");
 
     // Close the directory stream
     closedir(directory);
 
     if (errno != 0) {
-        perror("Error reading directory");
-        return EXIT_FAILURE;
+        perror(argv[0]);
+        return 1;
     }
 
-    return EXIT_SUCCESS;
+    return 0;
 }
